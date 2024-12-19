7fb65be7093c341af5f184e18ad3d757ee15be45
Dev Mik <mikdev4sec@gmail.com>
2024-12-19 23:30:48 +0300

    Создание приложений, объявление view-путей

--- blogicum/blog/apps.py
+++ blogicum/blog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BlogicumConfig(AppConfig):
+class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blogicum'
+    name = 'blog'
